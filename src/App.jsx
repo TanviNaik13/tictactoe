154d5f95e20b936b9ea5f2598495709eb1b24a8c
Tanvi Naik <21co67@aitdgoa.edu.in>
2024-07-10 17:08:32 +0530

    unused var set to warn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useState } from 'react';
+
 import Board from './components/Board';
+
 import './styles.scss';
+
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  console.log('hello');
+  const Btnclick = () => {
+    setCounter(currCounter => {
+      return currCounter + 1;
+    });
+  };
+
   return (
     <div className="app">
-      <Board />
+      <div>
+        <button onClick={Btnclick}>click me</button>
+        <div>{counter}</div>
+      </div>
     </div>
   );
 }
